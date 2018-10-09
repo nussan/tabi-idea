e04da4376fe6f3b5740eb008c6f0e48b3ef1a0a1
井澤諒哉 <izawa@izawaryoyanoMacBook-puro.local>
2018-10-09 15:57:48 +0900

    able to get eventlist only

--- tabiidea/app/src/main/java/checkers/tabi_idea/provider/Repository.kt
+++ tabiidea/app/src/main/java/checkers/tabi_idea/provider/Repository.kt
@@ -12,24 +12,33 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.IOException
 
 class Repository{
-    private var service: Service
+    private var userService: UserService
+    private var mmoService: MmoService
 
     init {
         val okHttpClient = OkHttpClient.Builder().build()
         val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
         val retrofit = Retrofit.Builder()
-                .baseUrl("http://192.168.10.4:3000/")
+                .baseUrl("https://damp-peak-50918.herokuapp.com/")
                 .addConverterFactory(MoshiConverterFactory.create(moshi))
                 .client(okHttpClient)
                 .build()
-        service = retrofit.create(Service::class.java)
+        //mock
+        val retrofit2 = Retrofit.Builder()
+                .baseUrl("http://192.168.10.3:3000/")
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .client(okHttpClient)
+                .build()
+        userService = retrofit.create(UserService::class.java)
+        mmoService = retrofit2.create(MmoService::class.java)
     }
 
     //userのlistをget
-    fun getUser(callback: (User) -> Unit){
-        service.user().enqueue(object : Callback<User> {
+    fun getUserCallback(callback: (User) -> Unit){
+        userService.user().enqueue(object : Callback<User> {
             override fun onResponse(call: Call<User>?, response: Response<User>?) {
                 Log.d("tubasa" , "success")
                 response?.let {
@@ -61,6 +70,37 @@ class Repository{
                                 Event(2,"USA", mutableListOf(), mutableListOf())
                         ))
                 callback(user)
+            }
+        })
+    }
+    //特定のmmoをget
+    fun getMmoCallback(callback: (List<MindMapObject>) -> Unit){
+        Log.d("tubasa2", "success")
+        mmoService.mmo().enqueue(object : Callback<List<MindMapObject>> {
+            override fun onResponse(call: Call<List<MindMapObject>>?, response: Response<List<MindMapObject>>?) {
+                Log.d("tubasa2" , "${response}")
+                response?.let {
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            callback(it)
+                        }
+                    }
+                }
+            }
+            override fun onFailure(call: Call<List<MindMapObject>>?, t: Throwable?) {
+                Log.d("tubasa2","cannot connect")
+                val mindmapobject: List<MindMapObject> = mutableListOf(
+                                        MindMapObject(0, "旅行", 1f / 2, 1f / 2, mutableListOf(1, 2, 3, 4)),
+                                        MindMapObject(1, "行先", 1f / 2, 1f / 4, mutableListOf(5, 6, 7, 8)),
+                                        MindMapObject(2, "予算", 1f / 4, 1f / 2, mutableListOf()),
+                                        MindMapObject(3, "食事", 1f / 2, 3f / 4, mutableListOf()),
+                                        MindMapObject(4, "宿泊", 3f / 4, 1f / 2, mutableListOf()),
+                                        MindMapObject(5, "熊本", 1f / 3, 1f / 15, mutableListOf()),
+                                        MindMapObject(6, "山口", 3f / 4, 1f / 5, mutableListOf()),
+                                        MindMapObject(7, "井澤", 1f / 4, 1f / 5, mutableListOf()),
+                                        MindMapObject(8, "瀧川", 5f / 8, 1f / 13f, mutableListOf())
+                                )
+                callback(mindmapobject)
             }
         })
     }
