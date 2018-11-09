5e3dffc05a03b5f34aefbdb20ec6de4f9f850b6a
rywyr <ginntama1996@hotmail.co.jp>
2018-11-09 16:19:34 +0900

    imageView add

--- tabiidea/app/src/main/java/checkers/tabi_idea/custom/view/RoundTextView.kt
+++ tabiidea/app/src/main/java/checkers/tabi_idea/custom/view/RoundTextView.kt
@@ -17,7 +17,7 @@ class RoundTextView : AppCompatTextView {
 
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         setBackgroundColor(Color.parseColor("#00CED1"))
-        elevation = 30f
+        elevation = 10f
         outlineProvider = object : ViewOutlineProvider() {
             override fun getOutline(view: View, outline: Outline) {
                 outline.setOval(0, 0, width, height)
