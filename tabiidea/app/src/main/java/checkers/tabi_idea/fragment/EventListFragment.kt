23dbcedf7583e10bb7883cda1f06bf75143e6b2a
Fumiya TAKIKAWA <takikawa@mk.ces.kyutech.ac.jp>
2018-11-10 22:22:47 +0900

    fix lateinit bug

--- tabiidea/app/src/main/java/checkers/tabi_idea/fragment/EventListFragment.kt
+++ tabiidea/app/src/main/java/checkers/tabi_idea/fragment/EventListFragment.kt
@@ -7,11 +7,8 @@ import checkers.tabi_idea.data.User
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.*
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.EditText
 import checkers.tabi_idea.R
 import checkers.tabi_idea.data.Event
@@ -23,8 +20,8 @@ import java.util.*
 
 class EventListFragment : Fragment() {
     private val eventManager = EventManager()
-    private var event_id = 0
-    private var event_password: String? = null
+    private var eventId = 0
+    private var eventPassword: String? = null
     private var mindMapObjectList: MutableList<MindMapObject> = mutableListOf(
             MindMapObject(1, "行先", 200f, 200f, 0),
             MindMapObject(2, "予算", 200f, -200f, 0),
@@ -38,6 +35,7 @@ class EventListFragment : Fragment() {
         super.onCreate(savedInstanceState)
         arguments?.let {
             userId = it.getInt("userId")
+            myuser = it.getParcelable("user")
             eventManager.eventList = it.getParcelableArrayList<Event>("eventListKey") as MutableList<Event>
         }
     }
@@ -102,12 +100,12 @@ class EventListFragment : Fragment() {
                             "title" to "${inputText.text}"
                     )
                     repository.addEvent(userId, title) {
-                        event_id = it.id
-                        event_password = it.password
+                        eventId = it.id
+                        eventPassword = it.password
                         Log.d("tubasa", it.id.toString())
-                        repository.addEventtoFb(event_id.toString())//event.id
+                        repository.addEventtoFb(eventId.toString())//event.id
                         mindMapObjectList.forEach {
-                            repository.addMmo(event_id.toString(), it)
+                            repository.addMmo(eventId.toString(), it)
                         }
                         eventManager.add(it)
                         eventListView.adapter.notifyDataSetChanged()
@@ -130,7 +128,7 @@ class EventListFragment : Fragment() {
         fun newInstance(user: User, eventList: MutableList<Event>) = EventListFragment().apply {
             arguments = Bundle().apply {
                 putInt("userId", user.id)
-                myuser = user
+                putParcelable("user", user)
                 putParcelableArrayList("eventListKey", ArrayList(eventList))
             }
         }
